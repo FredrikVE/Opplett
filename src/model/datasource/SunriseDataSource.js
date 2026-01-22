dba165acd09db2147d026b9a76ea2f250b381e6d
Fredrik Vogth-Eriksen <fredrikvogth@hotmail.com>
2026-01-22 15:15:50 +0100

    legg inn datsource og repo for sunrise API

---
+++ src/model/datasource/SunriseDataSource.js
@@ -0,0 +1,16 @@
+// src/model/datasource/SunriseDataSource.js
+import DataSource from "./DataSource.js";
+
+export default class SunriseDataSource extends DataSource {
+
+    async fetchSunrise(lat, lon, dateISO, offset) {
+        const path =
+            `weatherapi/sunrise/3.0/sun` +
+            `?lat=${lat}` +
+            `&lon=${lon}` +
+            `&date=${dateISO}` +
+            `&offset=${encodeURIComponent(offset)}`;
+
+        return this.get(path);
+    }
+}
---
+++ src/model/repositories/SunriseRepository.js
@@ -0,0 +1,47 @@
+// SunriseRepository.js
+export default class SunriseRepository {
+
+    constructor(sunriseDataSource) {
+        this.datasource = sunriseDataSource;
+    }
+
+    async getSunTimes(lat, lon, dateISO, offset) {
+
+        const result = await this.datasource.fetchSunrise(lat, lon, dateISO, offset);
+        const properties = result.properties;
+
+        return {
+            // Sunrise-API returnerer allerede lokal tid
+            sunrise: properties.sunrise?.time?.slice(11, 16) ?? null,
+            sunset: properties.sunset?.time?.slice(11, 16) ?? null
+        };
+    }
+}
+
+
+// src/test/testSunriseMain.js
+import SunriseDataSource from "../datasource/SunriseDataSource.js"
+
+async function testSunrise() {
+    const lat = 59.86;
+    const lon = 10.82;
+    const dateISO = "2026-01-22";
+    const offset = "+01:00";
+
+    const datasource = new SunriseDataSource();
+    const repository = new SunriseRepository(datasource);
+
+    try {
+        const sunTimes = await repository.getSunTimes(lat, lon, dateISO, offset);
+
+        console.log("Soldata:");
+        console.log(`\t- Soloppgang: ${sunTimes.sunrise}`);
+        console.log(`\t- Solnedgang: ${sunTimes.sunset}`);
+        console.log();
+    } catch (error) {
+        console.error("Feil ved henting av soldata:");
+        console.error(error);
+    }
+}
+
+testSunrise();
