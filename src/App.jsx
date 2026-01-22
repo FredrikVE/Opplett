f6ffbaeb2d7f2489c21ae7c68cab1663ed35fc12
Fredrik Vogth-Eriksen <fredrikvogth@hotmail.com>
2026-01-22 16:46:22 +0100

    fjern kommentar

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import "./ui/style/HomeScreen.css"
 
 import LocationForecastDataSource from "./model/datasource/LocationForecastDataSource.js"
 import LocationForecastRepository from "./model/repositories/LocationForecastRepository.js";
-//import HomeScreenViewModel from "./ui/viewmodel/HomeScreenViewModel.js";
 import HomeScreenViewModel from "./ui/viewmodel/HomeScreenViewModel.js";
 import HomeScreen from "./ui/view/HomeScreen.jsx";
 import SunriseRepository from "./model/repositories/SunriseRepository.js";
@@ -12,10 +11,10 @@ import SunriseDataSource from "./model/datasource/SunriseDataSource.js";
 
 export default function App() {
 
-    const lat = 59.86;
-    const lon = 10.82;
-    //const lat = 27.777835
-    //const lon = -15.692579
+    //const lat = 59.86;
+    //const lon = 10.82;
+    const lat = 27.777835
+    const lon = -15.692579
     const hoursAhead = 12;
 
     const locationForecastDatasource = new LocationForecastDataSource();
