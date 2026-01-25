f31360547ef542cb4d9440571881b609c2bb1822
Fredrik Vogth-Eriksen <fredrikvogth@hotmail.com>
2026-01-25 14:58:00 +0100

    Style søkefelt bedre

--- src/ui/view/components/SearchField.jsx
+++ src/ui/view/components/SearchField.jsx
@@ -1,19 +1,30 @@
-//src/ui/view/components/SearchField.jsx
+// src/ui/view/components/SearchField.jsx
 export default function SearchField({ query, suggestions, onSearchChange, onSuggestionSelected }) {
     return (
         <div className="search">
+            <label className="visually-hidden">
+                Søk etter sted, by eller adresse
+            </label>
+
             <input
+                id="location-search"
+                className="search-input"
                 type="text"
                 placeholder="Søk sted, by eller adresse…"
                 value={query}
                 onChange={(e) => onSearchChange(e.target.value)}
+
             />
 
             {suggestions.length > 0 && (
-                <ul className="search-suggestions">
+                <ul
+                    id="search-suggestions"
+                    className="search-suggestions"
+                >
                     {suggestions.map((s) => (
                         <li
                             key={`${s.lat}-${s.lon}`}
+                            className="search-suggestion"
                             onClick={() => onSuggestionSelected(s)}
                         >
                             {s.name}
