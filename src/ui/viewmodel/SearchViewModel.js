9ca880eebae4cf109db3c230e721dbf4db4704de
Fredrik Vogth-Eriksen <fredrikvogth@hotmail.com>
2026-01-27 08:46:58 +0100

    legg inn debounce i viewmodel

--- src/ui/viewmodel/SearchViewModel.js
+++ src/ui/viewmodel/SearchViewModel.js
@@ -1,37 +1,62 @@
 // src/ui/viewmodel/SearchViewModel.js
-import { useState } from "react";
+import { useRef, useState } from "react";
 
 export default function useSearchViewModel(geocodingRepository, onLocationSelected) {
-
-    // Statevariabeler for søk
+    
+    //Statevariabel og settmetode for søkemekanikk
     const [query, setQuery] = useState("");
     const [suggestions, setSuggestions] = useState([]);
 
-    async function onSearchChange(text) {
+    //debounce for å unngå for mange api-kall når man søke b, be, ber, berg, bergen (osv)
+    const debounceRef = useRef(null);
+    const abortRef = useRef(null);
+
+    const onSearchChange = (text) => {
         setQuery(text);
+
+        if (debounceRef.current) {
+            clearTimeout(debounceRef.current);
+        }
+
+        if (abortRef.current) {
+            abortRef.current.abort(); // avbryt forrige kall
+        }
 
         //Hvis teksten er kortere enn tre bokstaver forblir autocompletefeltet tomt
         if (text.length < 3) {
             setSuggestions([]);
             return;
         }
+        
+        //Legg inn en liten debounce og abortcontroller
+        debounceRef.current = setTimeout(async () => {
+            const controller = new AbortController();
+            abortRef.current = controller;
+        
+            try {
+                const results = await geocodingRepository.getSuggestions(text, controller.signal); //legger inn avbryt signal 
+                setSuggestions(results);
+            } 
+            catch (error) {
+                //Legg inn feilmelding for Abort som kan skje når man skriver fort
+                if (error?.name !== "AbortError") {
+                    console.warn("Søk feilet:", error);
+                }
+            }}, 
 
-        //Når mer enn tre bokstaver er skrevet inn, så hentes søkeforslag fra autocomplete
-        const results = await geocodingRepository.getSuggestions(text);
-        setSuggestions(results);
+        350); // 350 ms debounce
     }
-
-    //Funksjon som setter koordinatene fra autocomplete forslag
-    function onSuggestionSelected(suggestion) {
-        onLocationSelected({
-            lat: suggestion.lat,
-            lon: suggestion.lon,
-            name: suggestion.name
+    
+    const onSuggestionSelected = (suggestion) => {
+        onLocationSelected({ 
+            lat: suggestion.lat, 
+            lon: suggestion.lon, 
+            name: suggestion.name 
         });
 
         setQuery(suggestion.name);
         setSuggestions([]);
     }
-
+    
     return { query, suggestions, onSearchChange, onSuggestionSelected };
 }
