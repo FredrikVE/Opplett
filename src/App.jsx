ef1d8f4533761bb3582b92ca8af9e1e923d07f8a
Fredrik Vogth-Eriksen <fredrikvogth@hotmail.com>
2026-02-16 13:36:04 +0100

    fjern utkommentert kode

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,6 @@ import "./ui/style/NowCard.css"
 import "./ui/style/UVNowBar.css";
 
 //Navigation
-//import Navigation from "./navigation/Navigation.jsx";
 import { NAV_SCREENS } from "./navigation/navGraph.js";
 
 //DataSources
@@ -93,13 +92,6 @@ export default function App() {
 	return (
 		<>
 			<Header />
-			{/** 
-			<Navigation
-				activeScreen={activeScreen}
-				onChangeScreen={setActiveScreen}
-				SCREENS={NAV_SCREENS}
-			/>
-			*/}
 
 			{activeScreen === NAV_SCREENS.TABLE && (
 				<HomePage
