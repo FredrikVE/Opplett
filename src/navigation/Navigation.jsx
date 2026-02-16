4779fd23ad82cd88d232c2be4291a8d8915bcf80
Fredrik Vogth-Eriksen <fredrikvogth@hotmail.com>
2026-02-16 10:54:56 +0100

    legg inn navigasjon

---
+++ src/navigation/Navigation.jsx
@@ -0,0 +1,22 @@
+//src/navigation/Navigation.jsx
+export default function Navigation({ activeScreen, onChangeScreen, SCREENS }) {
+  return (
+    <div className="view-mode-selector">
+      <div className="toggle-container">
+        <button
+          className={activeScreen === SCREENS.TABLE ? "active" : ""}
+          onClick={() => onChangeScreen(SCREENS.TABLE)}
+        >
+          Tabell
+        </button>
+
+        <button
+          className={activeScreen === SCREENS.GRAPH ? "active" : ""}
+          onClick={() => onChangeScreen(SCREENS.GRAPH)}
+        >
+          Graf
+        </button>
+      </div>
+    </div>
+  );
+}
---
+++ src/navigation/navGraph.js
@@ -0,0 +1,5 @@
+//src/navigation/navGraph.js
+export const NAV_SCREENS = {
+  TABLE: "table",
+  GRAPH: "graph"
+};
