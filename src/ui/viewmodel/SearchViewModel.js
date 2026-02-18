7f64ef1c36456d3d346804b56719733067a61664
Fredrik Vogth-Eriksen <fredrikvogth@hotmail.com>
2026-02-18 11:17:16 +0100

    Fjern tids codesmell

--- src/ui/viewmodel/SearchViewModel.js
+++ src/ui/viewmodel/SearchViewModel.js
@@ -83,7 +83,7 @@ export default function useSearchViewModel(geocodingRepository, onLocationSelect
             lat: lat,
             lon: lon,
             name: null, 
-            timezone: Intl.DateTimeFormat().resolvedOptions().timeZone,
+            timezone: null
         });
     };
 
