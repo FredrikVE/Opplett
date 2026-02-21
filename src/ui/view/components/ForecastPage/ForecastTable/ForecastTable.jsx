a05dd646caae60ac0e56119fb650fa632a2271b9
Fredrik Vogth-Eriksen <fredrikvogth@hotmail.com>
2026-02-21 15:14:48 +0100

    Refakturer

--- src/ui/view/components/ForecastPage/ForecastTable/ForecastTable.jsx
+++ src/ui/view/components/ForecastPage/ForecastTable/ForecastTable.jsx
@@ -5,75 +5,75 @@ import { formatPrecipitation } from "../../../../utils/ForecastUtils/formatPreci
 import WindArrow from "../../Common/WindArrow/WindArrow.jsx";
 
 export default function ForecastTable({ forecast }) {
-    return (
-        <table className="forecast-table">
-            <thead>
-                <tr>
-                    <th>Tid</th>
-                    <th>Vær</th>
-                    <th>Temp</th>
-                    <th>Nedbør</th>
-                    <th>Vind</th>
-                    <th>Vindbeskrivelse</th>
-                </tr>
-            </thead>
+	return (
+		<table className="forecast-table">
+			<thead>
+				<tr>
+					<th>Tid</th>
+					<th>Vær</th>
+					<th>Temp</th>
+					<th>Nedbør</th>
+					<th>Vind</th>
+					<th>Vindbeskrivelse</th>
+				</tr>
+			</thead>
 
-            <tbody>
-                {forecast.map((item) => {
-                    const iconFile = getWeatherIconFileName(item.weatherSymbol);
-                    const windSpeed = item.wind;
-                    const gust = item.details?.wind_speed_of_gust;
-                    const windDir = item.details?.wind_from_direction;
+			<tbody>
+				{forecast.map((item) => {
+					const iconFile = getWeatherIconFileName(item.weatherSymbol);
+					const windSpeed = item.wind;
+					const gust = item.details?.wind_speed_of_gust;
+					const windDir = item.details?.wind_from_direction;
 
-                    //Generer beskrivelsen basert på SNL-definisjoner
-                    const windDesc = getWindSpeedDescription(windSpeed);
-                    const dirText = getWindDirectionText(windDir);
+					//Generer beskrivelsen basert på SNL-definisjoner
+					const windDesc = getWindSpeedDescription(windSpeed);
+					const dirText = getWindDirectionText(windDir);
 
-                    let fullDesc = `${windDesc} fra ${dirText}`;
-                    if (gust > windSpeed) {
-                        fullDesc += ` med vindkast på ${Math.round(gust)} m/s`;
-                    }
+					let fullDesc = `${windDesc} fra ${dirText}`;
+					if (gust > windSpeed) {
+						fullDesc += ` med vindkast på ${Math.round(gust)} m/s`;
+					}
 
-                    return (
-                        <tr key={`${item.dateISO}-${item.localTime}`}>
-                            <td className="time">{item.localTime}</td>
-                            
-                            <td className="weather-cell">
-                                {iconFile && (
-                                    <img
-                                        src={`/weather_icons/200/${iconFile}`}
-                                        alt={item.weatherSymbol}
-                                        className="weather-icon"
-                                        loading="lazy"
-                                    />
-                                )}
-                            </td>
+					return (
+						<tr key={`${item.dateISO}-${item.localTime}`}>
+							<td className="time">{item.localTime}</td>
+							
+							<td className="weather-cell">
+								{iconFile && (
+									<img
+										src={`/weather_icons/200/${iconFile}`}
+										alt={item.weatherSymbol}
+										className="weather-icon"
+										loading="lazy"
+									/>
+								)}
+							</td>
 
-                            <td className={`temperature ${item.temp < 0 ? 'is-minus' : 'is-plus'}`}>
-                                {Math.round(item.temp)}°
-                            </td>
-                            
-                            <td className="precipitation">
-                                {formatPrecipitation(item.precipitation)}
-                            </td>
+							<td className={`temperature ${item.temp < 0 ? 'is-minus' : 'is-plus'}`}>
+								{Math.round(item.temp)}°
+							</td>
+							
+							<td className="precipitation">
+								{formatPrecipitation(item.precipitation)}
+							</td>
 
-                            <td className="wind">
-                                <div className="wind-container">
-                                    <span>
-                                        {Math.round(windSpeed)}
-                                        {gust != null && ` (${Math.round(gust)})`}
-                                    </span>
-                                    <WindArrow degrees={windDir} size={16} />
-                                </div>
-                            </td>
+							<td className="wind">
+								<div className="wind-container">
+									<span>
+										{Math.round(windSpeed)}
+										{gust != null && ` (${Math.round(gust)})`}
+									</span>
+									<WindArrow degrees={windDir} size={16} />
+								</div>
+							</td>
 
-                            <td className="wind-description">
-                                {fullDesc}
-                            </td>
-                        </tr>
-                    );
-                })}
-            </tbody>
-        </table>
-    );
+							<td className="wind-description">
+								{fullDesc}
+							</td>
+						</tr>
+					);
+				})}
+			</tbody>
+		</table>
+	);
 }
