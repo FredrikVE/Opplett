754fdbea7de74d8d4c81ce745c885ff16b2aa3ec
Fredrik Vogth-Eriksen <fredrikvogth@hotmail.com>
2026-02-21 20:20:05 +0100

    Refakturer Filter

--- src/ui/view/components/AlertPage/FilterSelect.jsx
+++ src/ui/view/components/AlertPage/FilterSelect.jsx
@@ -1,169 +1,90 @@
 import { useState } from "react";
 import ChevronIcon from "../Common/Buttons/ChevronIcon.jsx";
-import "../../../style/FilterDropDown.css";
+import CheckBoxItem from "./CheckBoxItem.jsx";
+
 
 export default function FilterSelect({ value, onChange, options, defaultLabel, getCountForOption }) {
     const [isOpen, setIsOpen] = useState(false);
 
-    function isValueSelected(optionValue) {
-        let selected = false;
+    //Handler funksjoner
+    const handleOptionClick = (optionValue) => {
+        let newValue;
 
-        for (let i = 0; i < value.length; i++) {
-            if (value[i] === optionValue) {
-                selected = true;
-            }
+        if (value.includes(optionValue)) {
+            newValue = value.filter((v) => v !== optionValue);	//Fjerner verdien hvis den allerede finnes
+        } 
+		
+		else {												//Ellers Legger vi til verdien
+            newValue = [...value, optionValue];
         }
 
-        return selected;
-    }
-
-    function buildNewValue(optionValue) {
-        let newValue = [];
-        let alreadySelected = isValueSelected(optionValue);
-
-        if (alreadySelected === true) {
-            for (let i = 0; i < value.length; i++) {
-                if (value[i] !== optionValue) {
-                    newValue[newValue.length] = value[i];
-                }
-            }
-        } else {
-            for (let i = 0; i < value.length; i++) {
-                newValue[newValue.length] = value[i];
-            }
-
-            newValue[newValue.length] = optionValue;
-        }
-
-        return newValue;
-    }
-
-    function toggleOption(optionValue) {
-        const newValue = buildNewValue(optionValue);
         onChange(newValue);
-    }
+	}
 
-    function clearAll() {
+    const handleClearAll = () => {
         onChange([]);
     }
 
-    function getOptionValue(option) {
-        if (option.id) {
-            return option.id;
-        }
-
-        return option.value;
+    const handleToggleDropdown = () => {
+        setIsOpen(!isOpen);
     }
 
-    function getOptionLabel(option) {
-        if (option.displayName) {
-            return option.displayName;
+    const handleBlur = (event) => {
+        if (!event.currentTarget.contains(event.relatedTarget)) {		//Lukker bare hvis fokus flyttes helt ut av komponenten
+            setIsOpen(false);
         }
+    }
 
-        return option.label;
+    //Hjelpefunksjoner
+    function getOptionData(option) {
+        return {
+            id: option.id || option.value,
+            label: option.displayName || option.label
+        };
     }
 
     function getDisplayLabel() {
-        const selectedCount = value.length;
-
-        if (selectedCount === 0) {
+        if (value.length === 0) {
             return defaultLabel;
         }
-
-        return selectedCount + " valgt";
-    }
-
-    function handleToggleOpen() {
-        if (isOpen === true) {
-            setIsOpen(false);
-        } else {
-            setIsOpen(true);
-        }
-    }
-
-    function handleBlur(e) {
-        if (!e.currentTarget.contains(e.relatedTarget)) {
-            setIsOpen(false);
-        }
-    }
-
-    function buildOptionElements() {
-        const elements = [];
-
-        for (let i = 0; i < options.length; i++) {
-            const option = options[i];
-            const optionValue = getOptionValue(option);
-            const label = getOptionLabel(option);
-            const checked = isValueSelected(optionValue);
-
-            let hasAlerts = false;
-
-            if (getCountForOption) {
-                const count = getCountForOption(optionValue);
-
-                if (count > 0) {
-                    hasAlerts = true;
-                }
-            }
-
-            let spanClass = "";
-
-            if (hasAlerts === true) {
-                spanClass = "has-alerts";
-            }
-
-            elements[elements.length] = (
-                <div
-                    key={optionValue}
-                    className="custom-option"
-                    onClick={() => toggleOption(optionValue)}
-                >
-                    <input
-                        type="checkbox"
-                        checked={checked}
-                        readOnly
-                    />
-                    <span className={spanClass}>{label}</span>
-                </div>
-            );
-        }
-
-        return elements;
-    }
-
-    let dropdownContent = null;
-
-    if (isOpen === true) {
-        dropdownContent = (
-            <div className="custom-select-dropdown">
-                <div className="custom-option" onClick={clearAll}>
-                    <input
-                        type="checkbox"
-                        checked={value.length === 0}
-                        readOnly
-                    />
-                    <span>{defaultLabel}</span>
-                </div>
-                {buildOptionElements()}
-            </div>
-        );
+        return `${value.length} valgt`;
     }
 
     return (
-        <div
-            className="custom-select-wrapper"
-            tabIndex={0}
-            onBlur={handleBlur}
-        >
-            <button
-                className="custom-select-trigger"
-                onClick={handleToggleOpen}
-            >
+        <div className="custom-select-wrapper" tabIndex={0} onBlur={handleBlur}>
+            <button className="custom-select-trigger" onClick={handleToggleDropdown}>
                 <span>{getDisplayLabel()}</span>
                 <ChevronIcon isOpen={isOpen} size={14} />
             </button>
 
-            {dropdownContent}
+            {isOpen && (
+                <div className="custom-select-dropdown">
+
+                    {/* Standardvalget (tøm filter) */}
+                    <CheckBoxItem
+                        label={defaultLabel} 
+                        isChecked={value.length === 0} 
+                        onClick={handleClearAll} 
+                    />
+
+                    {/* Mapper over opsjoner med eksplisitt logikk */}
+                    {options.map((option) => {
+                        const { id, label } = getOptionData(option);
+                        const isChecked = value.includes(id);
+                        const count = getCountForOption ? getCountForOption(id) : 0;
+
+                        return (
+                            <CheckBoxItem
+                                key={id}
+                                label={label}
+                                isChecked={isChecked}
+                                hasAlerts={count > 0}
+                                onClick={() => handleOptionClick(id)}
+                            />
+                        );
+                    })}
+                </div>
+            )}
         </div>
     );
 }
