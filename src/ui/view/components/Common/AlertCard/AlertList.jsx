e5d6ce6182949f2668a7091ea34950fc3666d2e6
Fredrik Vogth-Eriksen <fredrikvogth@hotmail.com>
2026-02-22 12:13:34 +0100

    bedre indentering

--- src/ui/view/components/Common/AlertCard/AlertList.jsx
+++ src/ui/view/components/Common/AlertCard/AlertList.jsx
@@ -4,43 +4,43 @@ import AlertCard from "./AlertCard.jsx";
 
 export default function AlertList({ alerts, formatLocalDateTime }) {
 
-    //State som holder styr på hvilken alert som er åpen og ikke
-    const [openAlertId, setOpenAlertId] = useState(null);
+	//State som holder styr på hvilken alert som er åpen og ikke
+	const [openAlertId, setOpenAlertId] = useState(null);
 
-    //Hvis ingen alerts, render vi ingenting
-    if (!alerts || alerts.length === 0) {
-        return null;
-    }
+	//Hvis ingen alerts, render vi ingenting
+	if (!alerts || alerts.length === 0) {
+		return null;
+	}
 
-    //Funksjon for å toggle et alertCard
-    const handleToggle = (clickedAlertId) => {
+	//Funksjon for å toggle et alertCard
+	const handleToggle = (clickedAlertId) => {
 
-        const isSameAlert = openAlertId === clickedAlertId;
+		const isSameAlert = openAlertId === clickedAlertId;
 
-        if (isSameAlert) {
-            setOpenAlertId(null);       //Lukker hvis samme er klikket på.
-        } 
-        else {
-            setOpenAlertId(clickedAlertId);  //Åpner ny hvis ny er klikket på.
-        }
-    }
+		if (isSameAlert) {
+			setOpenAlertId(null);       //Lukker hvis samme er klikket på.
+		} 
+		else {
+			setOpenAlertId(clickedAlertId);  //Åpner ny hvis ny er klikket på.
+		}
+	}
 
-    return (
-        <div className="alerts-list-container">
-            {alerts.map((alert) => {
+	return (
+		<div className="alerts-list-container">
+			{alerts.map((alert) => {
 
-                const isThisAlertOpen = openAlertId === alert.id;
+				const isThisAlertOpen = openAlertId === alert.id;
 
-                return (
-                    <AlertCard
-                        key={alert.id}
-                        alert={alert}
-                        isOpen={isThisAlertOpen}
-                        onToggle={() => handleToggle(alert.id)}
-                        formatLocalDateTime={formatLocalDateTime}
-                    />
-                );
-            })}
-        </div>
-    );
+				return (
+					<AlertCard
+						key={alert.id}
+						alert={alert}
+						isOpen={isThisAlertOpen}
+						onToggle={() => handleToggle(alert.id)}
+						formatLocalDateTime={formatLocalDateTime}
+					/>
+				);
+			})}
+		</div>
+	);
 }
