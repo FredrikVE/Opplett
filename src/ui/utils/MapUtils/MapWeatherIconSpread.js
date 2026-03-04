7b9913438c8a1d2759d43cde6564df47be7edaba
Fredrik Vogth-Eriksen <fredrikvogth@hotmail.com>
2026-03-04 14:39:22 +0100

    Fiks bedre spredning på værikoner på kart

--- src/ui/utils/MapUtils/MapWeatherIconSpread.js
+++ src/ui/utils/MapUtils/MapWeatherIconSpread.js
@@ -1,30 +1,30 @@
-//src/ui/utils/MapUtils/MapWeatherIconSpread.js
+// src/ui/utils/MapUtils/MapWeatherIconSpread.js
 import { MAP_ZOOM_LEVELS } from "./MapZoomLevels";
 
 export function calculateWeatherIconSpread(zoom) {
     switch (true) {
+        //Zoom <= 3 (Landsoversikt)
         case (zoom <= MAP_ZOOM_LEVELS.COUNTRY):
             return 2.5;
 
-        case (zoom <= MAP_ZOOM_LEVELS.CONTINENT_DETAIL):
-            return 1.2;
-
+        //Zoom <= 6 (Region/Fylke) - Tilsvarer ca gamle 5-7
         case (zoom <= MAP_ZOOM_LEVELS.REGION):
             return 0.5;
 
-        case (zoom <= MAP_ZOOM_LEVELS.SUB_REGION):
-            return 0.3;
-
+        //Zoom <= 8 (Kommune) - Tilsvarer ca gamle 9
         case (zoom <= MAP_ZOOM_LEVELS.COUNTY):
             return 0.15;
 
+        //Zoom <= 10 (Bydel/Distrikt) - Tilsvarer ca gamle 11
         case (zoom <= MAP_ZOOM_LEVELS.DISTRICT):
-            return 0.08;
-
-        case (zoom <= MAP_ZOOM_LEVELS.DEFAULT):
             return 0.04;
 
+        //Zoom <= 12 (By/Adresse - Her er du på bilde 2!)
+        case (zoom <= MAP_ZOOM_LEVELS.DEFAULT):
+            return 0.01;
+
+        //Alt dypere enn zoom 12 (f.eks STREET på 14)
         default:
-            return 0.001; // For gateplan og dypere zoom
+            return 0.003; 
     }
 }
